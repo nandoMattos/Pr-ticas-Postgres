dffa1648194e1e449f59a7103bb6391f266b1e26
Luiz Fernando Mattos <97343285+nandoMattos@users.noreply.github.com>
2022-12-07 21:23:12 -0300

    Create 10.sql

---
+++ exercicios-sql/10.sql
@@ -0,0 +1,5 @@
+INSERT INTO compras (id_cliente, id_produto) 
+  VALUES (
+  (SELECT id FROM clientes WHERE nome='Olga Cascais Fortunato'),
+  (SELECT id FROM produtos WHERE nome='Celular Topo de Linha')
+ );
